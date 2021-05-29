aa4a2289a3b9479d7c617374b67739b1a81fd93a
maksstepak <maks.stepak1@gmail.com>
2021-05-29 15:55:59 +0200

    Add Card model and migration

---
+++ database/migrations/2021_05_28_122102_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('column_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->date('due_date')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
